aedbd5d6e3ec1ae743cc9045e5f36c3031b6372d
Roubear Atef <roubear.atef@gmail.com>
2024-03-18 12:08:57 +0200

    add classes and text

---
+++ consoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+
+
+ class  World
+{
+    void printHelloWorld(){
+Console.WriteLine("Hello, World!");
+    }
+   
+}
+class iti{
+  void printHelloITI(){
+Console.WriteLine("Hello, ITI!");
+    }
+
+}
