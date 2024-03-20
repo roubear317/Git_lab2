919c7302bc0b1ed6fc6478df58b3e2be1b71be6e
Mina Samo <mainsamouel@gmail.com>
2024-03-20 13:20:09 +0200

    Wait for the conflict

--- consoleApp1/Program.cs
+++ consoleApp1/Program.cs
@@ -25,6 +25,7 @@ Console.WriteLine("Hello, git!");
     void PrintHelloFromBranch(){
       Console.WriteLine("Hello from branch");
       Console.WriteLine("Hello from Rouber inside here");
+      Console.Write("this is the end");
     }
 
 }
